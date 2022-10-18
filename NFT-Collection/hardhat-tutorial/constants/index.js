a2ccb17d8686cf31d300771ec1361a7c9c6d7a44
Tesss <jade.fance@gmail.com>
2022-10-18 18:10:29 -0500

    Update index.js

--- NFT-Collection/hardhat-tutorial/constants/index.js
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x479D1f840286456cE22C0B3c3f7358a6e7B8EB2d";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "kowa-howa-n6f2.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
